7daf631200806516f09db31d7307440407dd548c
Onkur Lal <onkur24lal@gmail.com>
2023-11-01 11:50:15 -0400

    centering

--- ghi/app/src/MainPage.js
+++ ghi/app/src/MainPage.js
@@ -1,6 +1,6 @@
 function MainPage() {
   return (
-    <div className="px-4 py-5 my-5 text-center text-white d-flex flex-column justify-content-center vh-100 ms-3">
+    <div className="px-4 py-5 my-5 text-center text-white hero">
       <h1 className="display-5 fw-bold">CarCar</h1>
       <div className="col-lg-6 mx-auto">
         <p className="lead mb-4">
